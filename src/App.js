87ec06713036395a2bb38dde936157be83bd148a
zeVACa <logunik@bk.ru>
2022-09-28 17:44:23 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input className="new-todo" placeholder="What needs to be done?" />
+        </header>
+        <section className="main">
+          <ul className="todo-list">
+            <li className="completed">
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>
+                  <span className="description">Completed task</span>
+                  <span className="created">created 17 seconds ago</span>
+                </label>
+                <button className="icon icon-edit"></button>
+                <button className="icon icon-destroy"></button>
+              </div>
+            </li>
+            <li className="editing">
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>
+                  <span className="description">Editing task</span>
+                  <span className="created">created 5 minutes ago</span>
+                </label>
+                <button className="icon icon-edit"></button>
+                <button className="icon icon-destroy"></button>
+              </div>
+              <input type="text" className="edit" value="Editing task" />
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>
+                  <span className="description">Active task</span>
+                  <span className="created">created 5 minutes ago</span>
+                </label>
+                <button className="icon icon-edit"></button>
+                <button className="icon icon-destroy"></button>
+              </div>
+            </li>
+          </ul>
+          <footer className="footer">
+            <span className="todo-count">1 items left</span>
+            <ul className="filters">
+              <li>
+                <button className="selected">All</button>
+              </li>
+              <li>
+                <button>Active</button>
+              </li>
+              <li>
+                <button>Completed</button>
+              </li>
+            </ul>
+            <button className="clear-completed">Clear completed</button>
+          </footer>
+        </section>
+      </section>
     </div>
   );
 }
