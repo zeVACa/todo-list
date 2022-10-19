378512bdec1673ece22ce864c655f836ccab91e4
zeVACa <logunik@bk.ru>
2022-10-19 09:45:39 +0700

    footer hooks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import TasksFilter from '../TasksFilter';
 
-class Footer extends Component {
-  static defaultProps = {
-    setActiveFilterCategory: () => {},
-    activeFilterCategory: 'all',
-    deleteCompletedTasks: () => {},
-    uncompletedTasksCount: 0,
-  };
+const Footer = ({
+  setActiveFilterCategory,
+  activeFilterCategory,
+  deleteCompletedTasks,
+  uncompletedTasksCount,
+}) => {
+  return (
+    <footer className="footer">
+      <span className="todo-count">{uncompletedTasksCount} items left</span>
+      <TasksFilter
+        setActiveFilterCategory={setActiveFilterCategory}
+        activeFilterCategory={activeFilterCategory}
+      />
+      <button className="clear-completed" onClick={deleteCompletedTasks}>
+        Clear completed
+      </button>
+    </footer>
+  );
+};
 
-  static propTypes = {
-    setActiveFilterCategory: PropTypes.func,
-    activeFilterCategory: PropTypes.string,
-    deleteCompletedTasks: PropTypes.func,
-    uncompletedTasksCount: PropTypes.number,
-  };
+Footer.defaultProps = {
+  setActiveFilterCategory: () => {},
+  activeFilterCategory: 'all',
+  deleteCompletedTasks: () => {},
+  uncompletedTasksCount: 0,
+};
 
-  render() {
-    const {
-      setActiveFilterCategory,
-      activeFilterCategory,
-      deleteCompletedTasks,
-      uncompletedTasksCount,
-    } = this.props;
-
-    return (
-      <footer className="footer">
-        <span className="todo-count">{uncompletedTasksCount} items left</span>
-        <TasksFilter
-          setActiveFilterCategory={setActiveFilterCategory}
-          activeFilterCategory={activeFilterCategory}
-        />
-        <button className="clear-completed" onClick={deleteCompletedTasks}>
-          Clear completed
-        </button>
-      </footer>
-    );
-  }
-}
+Footer.propTypes = {
+  setActiveFilterCategory: PropTypes.func,
+  activeFilterCategory: PropTypes.string,
+  deleteCompletedTasks: PropTypes.func,
+  uncompletedTasksCount: PropTypes.number,
+};
 
 export default Footer;
