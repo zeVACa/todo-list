a843eddf21af4e2dd125b3365517f1c58d85289d
zeVACa <logunik@bk.ru>
2022-10-19 09:49:59 +0700

    filter hooks

--- src/components/TasksFilter/TasksFilter.js
+++ src/components/TasksFilter/TasksFilter.js
@@ -1,56 +1,48 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-class TasksFilter extends Component {
-  static defaultProps = {
-    setActiveFilterCategory: () => {},
-    activeFilterCategory: 'all',
-  };
+const TasksFilter = ({ setActiveFilterCategory, activeFilterCategory }) => {
+  const btnAllClass = classNames({
+    selected: activeFilterCategory === 'all',
+  });
 
-  static propTypes = {
-    setActiveFilterCategory: PropTypes.func,
-    activeFilterCategory: PropTypes.string,
-  };
+  const btnActiveClass = classNames({
+    selected: activeFilterCategory === 'active',
+  });
 
-  render() {
-    const { setActiveFilterCategory, activeFilterCategory } = this.props;
+  const btnCompletedClass = classNames({
+    selected: activeFilterCategory === 'completed',
+  });
 
-    const btnAllClass = classNames({
-      selected: activeFilterCategory === 'all',
-    });
+  return (
+    <ul className="filters">
+      <li>
+        <button className={btnAllClass} onClick={() => setActiveFilterCategory('all')}>
+          All
+        </button>
+      </li>
+      <li>
+        <button onClick={() => setActiveFilterCategory('active')} className={btnActiveClass}>
+          Active
+        </button>
+      </li>
+      <li>
+        <button onClick={() => setActiveFilterCategory('completed')} className={btnCompletedClass}>
+          Completed
+        </button>
+      </li>
+    </ul>
+  );
+};
 
-    const btnActiveClass = classNames({
-      selected: activeFilterCategory === 'active',
-    });
+TasksFilter.defaultProps = {
+  setActiveFilterCategory: () => {},
+  activeFilterCategory: 'all',
+};
 
-    const btnCompletedClass = classNames({
-      selected: activeFilterCategory === 'completed',
-    });
-
-    return (
-      <ul className="filters">
-        <li>
-          <button className={btnAllClass} onClick={() => setActiveFilterCategory('all')}>
-            All
-          </button>
-        </li>
-        <li>
-          <button onClick={() => setActiveFilterCategory('active')} className={btnActiveClass}>
-            Active
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setActiveFilterCategory('completed')}
-            className={btnCompletedClass}
-          >
-            Completed
-          </button>
-        </li>
-      </ul>
-    );
-  }
-}
+TasksFilter.propTypes = {
+  setActiveFilterCategory: PropTypes.func,
+  activeFilterCategory: PropTypes.string,
+};
 
 export default TasksFilter;
